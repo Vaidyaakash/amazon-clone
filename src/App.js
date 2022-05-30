8424246dbdd47c0e5f79b21a163e1e62d45fc4de
Vaidyaakash <akashvaidya132@gmail.com>
2022-05-30 15:05:00 +0530

    pending

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import './Style.css'
+import Student from "./Pages/Student";
+import Conatct from "./Pages/Conatct";
+import Homepage from "./Pages/Homepage";
+import AddStudent from "./Pages/AddStudent";
+import Edit from "./Pages/Edit";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/addstudent" element={<AddStudent />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/contact" element={<Conatct />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Conatct.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Conatct = () => {
+    return (
+        <>
+            <Nav />
+            <h1>Conatct</h1>
+        </>
+    )
+}
+
+export default Conatct
